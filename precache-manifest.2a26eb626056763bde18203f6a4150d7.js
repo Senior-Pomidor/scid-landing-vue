e18b65ca6c99317ab806bcc22f1949a06646b565
Senior-Pomidor <bobylev.vladi@gmail.com>
2022-07-23 00:01:00 +0300

    build

---
+++ precache-manifest.2a26eb626056763bde18203f6a4150d7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fec7754148f06c9a51d",
+    "url": "/scid-landing-vue/css/app.4bb84158.css"
+  },
+  {
+    "revision": "b66f32a515c249fc8414",
+    "url": "/scid-landing-vue/css/chunk-81ec992a.1dcbac27.css"
+  },
+  {
+    "revision": "bfb56c71e8d0870ff7701404504dce1d",
+    "url": "/scid-landing-vue/img/content/card-picture-plug.jpg"
+  },
+  {
+    "revision": "c3f5183fbf0104c67973d24e968b3603",
+    "url": "/scid-landing-vue/img/content/service-picture-1.jpg"
+  },
+  {
+    "revision": "933a0bb91123189c51b490351ac59691",
+    "url": "/scid-landing-vue/img/content/service-picture-2.jpg"
+  },
+  {
+    "revision": "28dcd239f30b419abc8fd0610d384749",
+    "url": "/scid-landing-vue/img/content/service-picture-3.jpg"
+  },
+  {
+    "revision": "6f93fe7df3e049c0de315852be0019f0",
+    "url": "/scid-landing-vue/img/content/service-picture-4.jpg"
+  },
+  {
+    "revision": "f76f753e58c851cad130cb4b615f87e6",
+    "url": "/scid-landing-vue/img/content/service-picture-5.jpg"
+  },
+  {
+    "revision": "e6a6214d27831f45c5529dc3bb5f13fe",
+    "url": "/scid-landing-vue/img/content/service-picture-6.jpg"
+  },
+  {
+    "revision": "9e5d2228c0771cf7f5993a5005ed2118",
+    "url": "/scid-landing-vue/img/content/slides/slide-1.jpg"
+  },
+  {
+    "revision": "15adbad3324075d27b5fe594e5c90493",
+    "url": "/scid-landing-vue/img/webfont.15adbad3.svg"
+  },
+  {
+    "revision": "9c95e3262b238f4c81cb7b18bfc99d9a",
+    "url": "/scid-landing-vue/index.html"
+  },
+  {
+    "revision": "9fec7754148f06c9a51d",
+    "url": "/scid-landing-vue/js/app.eae79232.js"
+  },
+  {
+    "revision": "b66f32a515c249fc8414",
+    "url": "/scid-landing-vue/js/chunk-81ec992a.86a4d403.js"
+  },
+  {
+    "revision": "f470ebed25dea81c344f",
+    "url": "/scid-landing-vue/js/chunk-vendors.76611197.js"
+  },
+  {
+    "revision": "fada280553b46b5d543df1236955b8f9",
+    "url": "/scid-landing-vue/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/scid-landing-vue/robots.txt"
+  }
+]);
